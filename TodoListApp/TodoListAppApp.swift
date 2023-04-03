008d9a83dc55c5717e82fc47f308f35aa6e43a5f
Aidan Kang <akang@lcs.on.ca>
2023-04-03 14:49:23 -0400

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Aidan Kang on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
